158e9d4df2dadd0ef4e63b0c9209ff835b821cbd
rossbencina <rossbencina@2ee31487-7f78-419f-0510-aa30afea7ba0>
2013-01-25 08:45:45 +0000

    added missing <cstring> header file

--- trunk/GroupServer.cpp
+++ trunk/GroupServer.cpp
@@ -26,6 +26,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include <algorithm>
 #include <iterator>
 #include <cassert>
+#include <cstring>
 
 #include "ip/NetworkingUtils.h"
 
