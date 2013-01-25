89b2981f8c45b6d9e39ab0bab8f1a8c894a2f0c3
rossbencina <rossbencina@2ee31487-7f78-419f-0510-aa30afea7ba0>
2013-01-25 08:52:21 +0000

    added missing <cstdlib> header file

--- trunk/OscGroupClient.cpp
+++ trunk/OscGroupClient.cpp
@@ -26,7 +26,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include <ctime>
 #include <string>
 #include <cassert>
-
+#include <cstdlib>
 
 #include "osc/OscReceivedElements.h"
 #include "osc/OscOutboundPacketStream.h"
